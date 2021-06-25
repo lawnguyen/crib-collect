711c1b9a18b6d6594e67e3cfcfd3c4540130676a
lawnguyen <law.lnguyen@gmail.com>
2021-06-25 01:35:42 -0600

    Reformat CardAttributesList into a table

--- crib-collect/src/components/Attribute.js
+++ crib-collect/src/components/Attribute.js
@@ -3,9 +3,10 @@ import React from 'react';
 class Attribute extends React.Component {
   render() {
     return (
-        <div>
-            {this.props.name}: {this.props.value}
-        </div>
+      <tr>
+        <td>{this.props.name}</td>
+        <td>{this.props.value}</td>
+      </tr>
     )
   }
 }
--- crib-collect/src/components/CardAttributesList.js
+++ crib-collect/src/components/CardAttributesList.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import Attribute from './Attribute';
+import './CardAttributesList.css';
 
 class CardAttributes extends React.Component {
   render() {
     const attributes = this.props.attributes;
     return (
-      <div class="content">
-        {Object.keys(attributes).map(attr => (
-          <Attribute name={attr} value={attributes[attr]}></Attribute>
-        ))}
+      <div className="content">
+        <table className="table is-narrow">
+          <tbody>
+            {Object.keys(attributes).map(attr => (
+                <Attribute key={attr} name={attr} value={attributes[attr]}></Attribute>
+            ))}
+          </tbody>
+        </table>
       </div>
     )
   }
