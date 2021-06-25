bede6a2d33e92f7e81127cd905a2786746396fb6
lawnguyen <law.lnguyen@gmail.com>
2021-06-25 02:07:48 -0600

    map attribute names to more human-read-able labels

--- crib-collect/src/components/CardAttributesList.js
+++ crib-collect/src/components/CardAttributesList.js
@@ -5,12 +5,29 @@ import './CardAttributesList.css';
 class CardAttributes extends React.Component {
   render() {
     const attributes = this.props.attributes;
+    let attrToDisplayMap = {
+      bedrooms: { label: "beds", icon: '' },
+      bathrooms: { label: "baths", icon: '' },
+      sqft: { label: "sqft", icon: '' },
+      insuiteLaundry: { label: "w/d", icon: '' },
+      neighborhood: { label: "area", icon: '' },
+      parking: { label: "parking", icon: '' },
+      utilities: { label: "utilities", icon: '' },
+      buildingType: { label: "home type", icon: '' },
+      notes: { label: "notes", icon: '' }
+    };
+
     return (
       <div className="content">
         <table className="table">
           <tbody>
             {Object.keys(attributes).map(attr => (
-                <Attribute key={attr} name={attr} value={attributes[attr]}></Attribute>
+              <Attribute
+                key={attr}
+                icon={attrToDisplayMap[attr].icon}
+                name={attrToDisplayMap[attr].label}
+                value={attributes[attr]}>
+              </Attribute>
             ))}
           </tbody>
         </table>
