391f8722a470f4f5c333b4cd6ed1f0911853636e
lawnguyen <law.lnguyen@gmail.com>
2021-06-25 17:51:29 -0600

    Change footer text to icons

--- crib-collect/src/components/CardFooter.js
+++ crib-collect/src/components/CardFooter.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import deleteIcon from '../icons/delete.svg';
+import editIcon from '../icons/edit.svg';
 
 class CardFooter extends React.Component {
   render() {
     return (
       <footer className="card-footer">
-        <a href="#" className="card-footer-item">Edit</a>
-        <a href="#" className="card-footer-item">Delete</a>
+        <a className="card-footer-item">
+          <img src={editIcon}/>
+        </a>
+        <a className="card-footer-item">
+          <img src={deleteIcon}/>
+        </a>
       </footer>
     )
   }
