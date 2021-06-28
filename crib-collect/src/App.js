f77e4220986e0edeae66a5a36c9cbfcc443e484d
lawnguyen <law.lnguyen@gmail.com>
2021-06-27 23:59:44 -0600

    initial implementation of drag and drop

--- crib-collect/src/App.js
+++ crib-collect/src/App.js
@@ -1,21 +1,34 @@
 import './App.css';
 import React from 'react';
 import HomeCard from './components/HomeCard.js';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
 class App extends React.Component {
   render() {
     return (
       <div className="app">
-        <div className="columns is-multiline">
-
-          {this.props.homes.map(home => (
-            <div 
-              key={home.link} 
-              className="column is-one-quarter-fullhd is-half-tablet is-one-third-desktop">
-              <HomeCard homeDetails={home}></HomeCard>
-            </div>
-          ))}
-        </div>
+        <DragDropContext>
+          <Droppable droppableId="homes">
+            {(provided) => (
+              <ul {...provided.droppableProps} ref={provided.innerRef}>
+                {this.props.homes.map((home, index) => {
+                  return (
+                    <Draggable key={home.link} draggableId={home.link} index={index}>
+                      {(provided) => (
+                        <li 
+                          ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}
+                          className="column is-one-quarter-fullhd is-half-tablet is-one-third-desktop">
+                          <HomeCard homeDetails={home}></HomeCard>
+                        </li>
+                      )}
+                    </Draggable>
+                  )
+                })}
+                {provided.placeholder}
+              </ul>
+            )}
+          </Droppable>
+        </DragDropContext>
       </div>
     );
   }
