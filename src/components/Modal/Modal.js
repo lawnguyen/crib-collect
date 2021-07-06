5e996d4b73acdae14e4a685c601ba7ade5033fd7
lawnguyen <law.lnguyen@gmail.com>
2021-07-06 01:46:49 -0600

    add modal component

---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Modal.css";
+
+function Modal({ title, children }) {
+  return (
+    <div className="modal is-active">
+      <div className="modal-background"></div>
+      <div className="modal-content">
+        <article className="message is-info">
+          <div className="message-header">
+            <p>{title}</p>
+            <button className="delete" aria-label="delete"></button>
+          </div>
+          <div className="message-body">
+            {children}
+          </div>
+        </article>
+      </div>
+      <button className="modal-close is-large" aria-label="close"></button>
+    </div>
+  );
+}
+
+export default Modal;
