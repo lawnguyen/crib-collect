b6116afb562a4f96705d260bd14e3ff7af2f0e60
lawnguyen <law.lnguyen@gmail.com>
2021-07-07 16:05:21 -0600

    add checkBoxField component

--- src/components/HomeForm/CheckboxField/CheckboxField.js
+++ src/components/HomeForm/CheckboxField/CheckboxField.js
@@ -1,9 +1,19 @@
 import React from "react";
+import FieldLabel from "../FieldLabel/FieldLabel";
+import "./CheckboxField.css";
 
 function CheckboxField({ label, options }) {
   return (
     <div className="field">
-
+      <FieldLabel label={label}></FieldLabel>
+      {options.map((option) => {
+        return (
+          <label className="checkbox">
+            <input type="checkbox" />
+            {" " + option}
+          </label>
+        );
+      })}
     </div>
   );
 }
