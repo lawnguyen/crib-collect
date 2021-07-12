747b3a51741e3a8c6298cbd5bd981c30c20ca8df
lawnguyen <law.lnguyen@gmail.com>
2021-07-11 19:37:55 -0600

    prepare SelectField for editing

--- src/components/HomeForm/SelectField/SelectField.js
+++ src/components/HomeForm/SelectField/SelectField.js
@@ -1,12 +1,16 @@
 import React from "react";
 
-function SelectField({ options, onChange, name }) {
+function SelectField({ options, onChange, name, selected }) {
   return (
     <div className="field">
       <div className="select">
         <select onChange={onChange} name={name}>
           {options.map((option) => {
-            return <option key={option}>{option}</option>;
+            return option === selected ? (
+              <option selected key={option}>{option}</option>
+            ) : (
+              <option key={option}>{option}</option>
+            );
           })}
         </select>
       </div>
