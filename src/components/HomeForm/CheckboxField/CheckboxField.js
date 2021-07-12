cffd0b79ee228135d18b749053f6d64c12be3a6c
lawnguyen <law.lnguyen@gmail.com>
2021-07-11 19:37:25 -0600

    Prepare CheckboxField for editing

--- src/components/HomeForm/CheckboxField/CheckboxField.js
+++ src/components/HomeForm/CheckboxField/CheckboxField.js
@@ -1,11 +1,16 @@
 import React from "react";
 import "./CheckboxField.css";
 
-function CheckboxField({ options, onChange, name }) {
+function CheckboxField({ options, onChange, name, selectedList }) {
   return (
     <div className="field">
       {options.map((option) => {
-        return (
+        return selectedList && selectedList.includes(option) ? (
+          <label key={option} className="checkbox">
+            <input checked onChange={onChange} name={name} type="checkbox" />
+            {" " + option}
+          </label>
+        ) : (
           <label key={option} className="checkbox">
             <input onChange={onChange} name={name} type="checkbox" />
             {" " + option}
