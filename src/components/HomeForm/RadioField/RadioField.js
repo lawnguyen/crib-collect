151984d83f0d4cfef6f80effce72c19c909a1987
lawnguyen <law.lnguyen@gmail.com>
2021-07-11 19:37:44 -0600

    prepare RadioField for editing

--- src/components/HomeForm/RadioField/RadioField.js
+++ src/components/HomeForm/RadioField/RadioField.js
@@ -1,12 +1,17 @@
 import React from "react";
 import "./RadioField.css";
 
-function RadioField({ options, name, onChange }) {
+function RadioField({ options, name, onChange, selected }) {
   return (
     <div className="field">
       <div className="control">
         {options.map((option) => {
-          return (
+          return selected === option ? (
+            <label key={option} className="radio">
+              <input checked onChange={onChange} type="radio" name={name} />
+              {" " + option}
+            </label>
+          ) : (
             <label key={option} className="radio">
               <input onChange={onChange} type="radio" name={name} />
               {" " + option}
