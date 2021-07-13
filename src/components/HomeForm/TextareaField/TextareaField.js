ac8c4ecbd6dc05931dac37dbfa6df2aca9083094
lawnguyen <law.lnguyen@gmail.com>
2021-07-13 17:16:12 -0600

    add state value to textAreaField

--- src/components/HomeForm/TextareaField/TextareaField.js
+++ src/components/HomeForm/TextareaField/TextareaField.js
@@ -1,12 +1,13 @@
 import React from "react";
 
-function TextareaField({ placeholder, onChange, name }) {
+function TextareaField({ placeholder, onChange, name, value }) {
   return (
     <textarea
       onChange={onChange}
       name={name}
       className="textarea"
       placeholder={placeholder}
+      value={value}
     ></textarea>
   );
 }
