6a4ffc102a2acf9bc1f0f4380b8bd21f1da4d974
lawnguyen <law.lnguyen@gmail.com>
2021-07-25 18:50:14 -0600

    implement tooltip for CopyToClipboard component

--- src/components/CopyToClipboard/CopyToClipboard.js
+++ src/components/CopyToClipboard/CopyToClipboard.js
@@ -1,20 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import CopyIcon from "../../icons/copy.svg";
 import "./CopyToClipboard.css";
 
 function CopyToClipboard({ textToCopy }) {
-    console.log(textToCopy);
+    const [tooltipText, updateTooltipText] = useState("Copy to clipboard");
   return (
     <div className="control copy-field">
-      <input
-        className="input"
-        type="text"
-        value={textToCopy}
-        readOnly
-      />
-      <button className="copy-button">
-        <img src={CopyIcon} alt="Copy icon"></img>
-      </button>
+      <input className="input" type="text" value={textToCopy} readOnly />
+      <div className="tooltip">
+        <button
+          className="copy-button"
+          onClick={() => {
+            navigator.clipboard.writeText(textToCopy);
+            updateTooltipText("Copied!");
+          }}
+        >
+          <span className="tooltiptext">
+            {tooltipText}
+          </span>
+          <img src={CopyIcon} alt="Copy icon"></img>
+        </button>
+      </div>
     </div>
   );
 }
